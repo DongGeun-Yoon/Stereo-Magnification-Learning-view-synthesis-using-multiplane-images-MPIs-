65de12fb019b0c1293690bec05f236dc6081adcc
enfly903 <ehdrms903@gmail.com>
2022-07-27 13:55:26 +0900

    demo

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ This repository reproduces Pytorch implementation of ["Stereo Magnification: Lea
 
 The code is based on authors' Tensorflow implementation [here](https://github.com/google/stereo-magnification) and reproduce implementation [here](https://github.com/Findeton/mpi_vision). Thanks for sharing the codes.
 
+example images with the original tensorflow weight, which I have converted to PyTorch state dicts.
+
+<p align="center">
+  <img src="mpi_vision/render_00_-2.0.jpg" width="40%"/>
+  <img src="mpi_vision/render_02_-1.0.png" width="40%"/>
+  <img src="mpi_vision/render_06_1.0.png" width="40%"/>
+  <img src="mpi_vision/render_08_2.0.png" width="40%"/>
+</p>
+
 ## Datasets
 1. Download [real-estate-10k-dataset](https://google.github.io/realestate10k/).
 2. Set your dataset path like this:
--- demo.py
+++ demo.py
@@ -2,29 +2,140 @@ import os
 import PIL
 import base64
 import matplotlib.pyplot as plt
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 from argparse import ArgumentParser
-
-from dataset import RealEstateDataset
 from networks import StereoMagnificationModel
-
 from utils import *
 
-batch_size = 1
+Image1 = 'mpi_vision/bananas1.jpg'
+Image2 = 'mpi_vision/bananas2.jpg'
+Pose1 = '0.999947548 -0.001357046 -0.010151989 0.359417914 0.001413203 0.999983728 0.005526535 -0.003794850 0.010144324 -0.005540592 0.999933183 0.124850837'
+Pose2 = '0.999962628 -0.002106981 -0.008386925 0.376296459 0.002151508 0.999983609 0.005303705 -0.001512435 0.008375612 -0.005321552 0.999950767 0.124671650'
+xoffset = 0.017
+yoffset = 0.0
+zoffset = 0.0
+
+fx = 0.5
+fy = 0.5
+render_multiples = '-2,-1.5,-1,-0.5,0,0.5,1,1.5,2'
+render = True
+
+xshift = 0
+yshift = 0
+
 img_size = (512, 288)
+batch_size = 1
 checkpoint = 'pretrained/tf.pth'
-data_dir = "./real-estate-10k-run"
 save_dir = 'mpi_vision'
 
-parser = ArgumentParser(description="Train for MPIs")
+parser = ArgumentParser(description="Demo for MPIs")
 
-parser.add_argument('--data_dir', default=data_dir, type=str, help="Directory of real-estate data")
+parser.add_argument('--image1', default=Image1, type=str, help="Directory of ref image")
+parser.add_argument('--image2', default=Image2, type=str, help="Directory of src image")
+parser.add_argument('--pose1', default=Pose1, type=str, help="ref image pose")
+parser.add_argument('--pose2', default=Pose2, type=str, help="src image pose")
+parser.add_argument('--xoffset', default=xoffset, type=float, help="xoffset")
+parser.add_argument('--yoffset', default=yoffset, type=float, help="yoffset")
+parser.add_argument('--zoffset', default=zoffset, type=float, help="zoffset")
+parser.add_argument('--render_multiples', default=render_multiples, type=str, help="rendering parameters")
+parser.add_argument('--render', default=render, type=bool, help="render")
+parser.add_argument('--fx', default=fx, type=float, help="Focal length")
+parser.add_argument('--fy', default=fy, type=float, help="Focal length")
+parser.add_argument('--xshift', default=xshift, type=float, help="Horizontal pixel shift")
+parser.add_argument('--yshift', default=yshift, type=float, help="Vertical pixel shift")
+
+parser.add_argument('--save_dir', default=save_dir, type=str, help="Directory of save rendered view")
 parser.add_argument('--checkpoint', default=checkpoint, type=str, help="Directory of load checkpoint")
-parser.add_argument('--img_size', default=img_size, type=tuple, help="training data image resolution")
 parser.add_argument('--batch_size', default=batch_size, type=int, help="Mini batch size")
+
+
+def shift_image(image, x, y):
+    height, width, _ = image.shape # [h, w, 3]
+    x = int(round(x))
+    y = int(round(y))
+    dtype = image.dtype
+    if x > 0:
+        image = np.concatenate((np.zeros([height, x, 3], dtype=dtype), image[:, :(width-x)]), axis=1)
+    elif x < 0:
+        image = np.concatenate((image[:, -x:], np.zeros([height, -x, 3], dtype=dtype)), axis=1)
+    if y > 0:
+        image = np.concatenate((np.zeros([y, width, 3], dtype=dtype), image[:(height-y), :]), axis=0)
+    elif y < 0:
+        image = np.concatenate((image[-y:, :], np.zeros([-y, width, 3], dtype=dtype)), axis=0)
+    return image
+
+def load_image(path, padx, pady, xshift, yshift):
+    image = PIL.Image.open(path).convert('RGB')
+    image = np.array(image)
+    padded = np.pad(image, ((padx, padx), (pady, pady), (0,0)))
+    image = shift_image(padded, xshift, yshift)
+    return image
+
+def crop_to_multiple(image, size):
+    height, width, _ = image.shape
+    new_width = width - (width % size)
+    new_height = height - (height % size)
+
+    left = (width % size) // 2
+    right = new_width + left
+    top = (height % size) // 2
+    bottom = new_height + top
+    return image[top:bottom, left:right]
+
+def pose_from_args(pose):
+    values = [float(x) for x in pose.replace(',', ' ').split()]
+    assert len(values) == 12
+    return torch.Tensor([values[0:4], values[4:8], values[8:12], [0.0, 0.0, 0.0, 1.0]])
+
+def get_inputs(padx, pady):
+    inputs = {}
+    image1 = load_image(args.image1, padx, pady, 0, 0)
+    image2 = load_image(args.image2, padx, pady, -args.xshift, -args.yshift)
+
+    shape1_before_crop = image1.shape # [h, w, 3]
+    shape2_before_crop = image2.shape
+    image1 = crop_to_multiple(image1, 16)
+    image2 = crop_to_multiple(image2, 16)
+    shape1_after_crop = image1.shape
+    shape2_after_crop = image2.shape
+    
+    pose_one = pose_from_args(args.pose1).to(device)
+    pose_two = pose_from_args(args.pose2).to(device)
+
+    original_width = shape1_before_crop[1] - 2 * padx
+    original_height = shape1_before_crop[0] - 2 * pady
+    eventual_width = shape1_after_crop[1]
+    eventual_height = shape1_after_crop[0]
+    
+    fx = original_width * float(args.fx)
+    fy = original_height * float(args.fy)
+
+    cx = eventual_width * 0.5
+    cy = eventual_height * 0.5
+    
+    intrinsics = torch.Tensor([[fx, 0.0, cx],
+                               [0.0, fy, cy],
+                               [0.0, 0.0, 1.0]]).to(device)
+    tensor_image1 = preprocess_image_torch(torch.Tensor(image1)/255.).to(device)
+    tensor_image2 = preprocess_image_torch(torch.Tensor(image2)/255.).to(device)
+
+    psv_planes = torch.Tensor(inv_depths(1, 100, 32)).to(device)
+    curr_pose = torch.matmul(pose_two, torch.inverse(pose_one))
+    curr_psv = plane_sweep_torch_one(tensor_image2, psv_planes, curr_pose, intrinsics)
+    inputs = torch.cat([torch.unsqueeze(tensor_image1, 0), curr_psv], 3)
+    dev_var = {
+        'tgt_img_cfw': 1,
+        'tgt_img': 1,
+        'ref_img': tensor_image2,
+        'ref_img_wfc': pose_two,
+        'intrinsics': intrinsics,
+        'mpi_planes': psv_planes
+    }
+    return inputs.permute([0,3,1,2]), original_width, original_height, dev_var
 
 def get_base64_encoded_image(image_path):
     with open(image_path, "rb") as img_file:
@@ -32,11 +143,10 @@ def get_base64_encoded_image(image_path):
 
 if __name__ == '__main__':
     import time
-    
+    args = parser.parse_args()
     # load checkpoint
-    ckt = torch.load(checkpoint)
+    ckt = torch.load(args.checkpoint)
     num_planes = 32
-    
     model = StereoMagnificationModel(num_mpi_planes=num_planes)
     model.load_state_dict(ckt['state_dict'])
 
@@ -45,25 +155,47 @@ if __name__ == '__main__':
     model.eval()
 
     # Custom dataloaders
-    valid_dataset = RealEstateDataset(data_dir, img_size=img_size, is_valid=True, num_planes=num_planes)
-    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=8)
+    max_multiple = 0
+    if args.render:
+        render_list = [float(x) for x in args.render_multiples.split(',')]
+        max_multiple = max(abs(float(m)) for m in render_list)
+    padx = int(max_multiple * abs(args.xshift) + 8)
+    pady = int(max_multiple * abs(args.yshift) + 8)
     
+    print('Padding inputs: padx={}, pady={}, (multiple={})'.format(padx, pady, max_multiple))
     time1 = time.time()
-    for i, (img, dep) in enumerate(valid_loader):
-        img = img.type(torch.FloatTensor).to(device) 
-        target = dep['tgt_img'].to(device)
-        # Forward prop.
-        out = model(img)  
-        rgba_layers = mpi_from_net_output(out, dep)
-        rel_pose = torch.matmul(dep['tgt_img_cfw'], dep['ref_img_wfc']).to(device)
-        pred_image = mpi_render_view_torch(rgba_layers, rel_pose, dep['mpi_planes'][0], dep['intrinsics']).to(device)
+    inputs, original_width, original_height, dep = get_inputs(padx, pady)
+    inputs = inputs.type(torch.FloatTensor).to(device) 
 
-        # save image
-        for j in range(num_planes):
-            save_image( rgba_layers[0, :, :, j,:].cpu(), os.path.join(save_dir, "mpi{}.png".format(  ("0" + str(j))[-2:]  )))
+    # Forward prop.
+    out = model(inputs)  
+    rgba_layers = mpi_from_net_output(out, dep)
+    
+    print(time.time()-time1)
+    # save image
+    for j in range(num_planes):
+        save_image( rgba_layers[0, :, :, j,:].cpu(), os.path.join(args.save_dir, "mpi{}.png".format(  ("0" + str(j))[-2:]  )))
 
     print(time.time()-time1)
-    image_srcs = [get_base64_encoded_image(os.path.join(save_dir, 'mpi{}.png'.format( ("0" + str(i))[-2:] ))) for i in range(num_planes)]
+
+    render = {}
+    if args.render:
+        print('Rendering new views...')
+        for index, multiple in enumerate(render_list):
+            m = float(multiple)
+            print('    offset:{}'.format(multiple))
+            pose = torch.Tensor([[1.0, 0.0, 0.0, -m * args.xoffset], 
+                                 [0.0, 1.0, 0.0, -m * args.yoffset],
+                                 [0.0, 0.0, 1.0, -m * args.zoffset],
+                                 [0.0, 0.0, 0.0, 1.0]]).unsqueeze(0).cuda()
+            image = mpi_render_view_torch(rgba_layers, pose, dep['mpi_planes'], dep['intrinsics'].unsqueeze(0))
+            
+            normalized_img = (image[0,:,:,:3]+1.0)/2.0 # RGB
+            normalized_img = normalized_img.permute(2,0,1)
+            RGBImg = torchvision.transforms.ToPILImage()( normalized_img )
+            RGBImg.save(os.path.join(args.save_dir, 'render_{:02}_{}.png'.format(index, m)))
+
+    image_srcs = [get_base64_encoded_image(os.path.join(args.save_dir, 'mpi{}.png'.format( ("0" + str(i))[-2:] ))) for i in range(num_planes)]
 
     # make mpi viewer
     with open("./mpi_vision/deepview-mpi-viewer-template.html", "r") as template_file:
